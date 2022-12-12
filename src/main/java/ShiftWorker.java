8eb2aa6e4ba5775278fe3c9b2cb8d0d623f7e24e
ALEX CRONIN - STUDENT <g00406014@atu.ie>
2022-12-12 09:19:05 +0000

    seocnd code commited

--- src/main/java/ShiftWorker.java
+++ src/main/java/ShiftWorker.java
@@ -1,3 +1,23 @@
+<<<<<<< HEAD
 public class ShiftWorker {
+=======
+
+
+public class ShiftWorker {
+    private String name;
+    private String age;
+    private String gender;
+    private String shift;
+
+    public ShiftWorker(){
+    }
+    public ShiftWorker(String name,String age,String gender,String shift){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.shift = shift;
+    }
+
+>>>>>>> 4df8d7b... seocnd code commited
 
 }
